c42a14e533bb4c7794bdc9c3e4736d0e6295bfd2
ggtux <69096321+ggtux@users.noreply.github.com>
2023-10-23 05:43:00 +0200

    Add files via upload

--- datenoverlay.py
+++ datenoverlay.py
@@ -6,17 +6,28 @@ from numpy import sign
 from math import log10, exp, pow, log
 import requests
 import csv
+import time
+from datetime import datetime, timezone, timedelta
+import json
+
+
 
 # API key von www.weather.com 
-api_key = '46cf957af9f541488f957af9f561488e'
+api_key = '4f9ffdab37989a944d92a452e4277819'
 
 # base url variable to store url
-base_url = 'https://api.weather.com/v2/pws/observations/current?'
+base_url = 'https://api.openweathermap.org/data/3.0/onecall/timemachine?'
 
 # give city name 
-stationID = 'ILAUCH27'
+lat='51.4798'
+lon='13.7319'
+
+# Aktuelle Zeit als Unix-Zeitstempel
+dt = int(time.time())
+dts=str(dt)
+
 # complete_url variable to store
-complete_url = base_url + "stationId=" + stationID + "&format=json&units=m&apiKey=" + api_key
+complete_url = base_url + "lat="+lat + "&lon="+lon +"&dt=" + dts + "&units=metric" + "&appid=" + api_key
 
 #get method of requests module
 #return response object 
@@ -24,7 +35,7 @@ response = requests.get(complete_url)
 
 #json method of response object
 # convert json format data into python format data 
-observations = response.json()
+raw_data = response.json()
 
 # data from weatherradio
 wr_url="http://192.168.178.74"
@@ -46,25 +57,9 @@ K5 = 100.
 K6 = 0.
 K7 = 100.
 
-
 i=0 # Index for Messwerte
 Messwerte=[]
-
-# Werte in Liste Messwerte schreiben
-
-  	# Beobachtungsdaten ausgeben und schreiben
-for obs in observations['observations']:
-	for key, value in obs.items():
-		if key != 'metric':
-			Messwerte.insert(i,value)
-			i=i+1
-            	
-for obs in observations['observations']:
-	metric = obs['metric']
-	for key, value in metric.items():
-		Messwerte.insert(i,value)
-		i=i+1
-    		
+		
 		# Wetterdaten schreiben
 for sensor, data in weather['weather'].items():
 	if 'RG11 Rain Sensor' in sensor:
@@ -79,21 +74,50 @@ for sensor, data in weather['weather'].items():
 		for key, value in data.items():
 			Messwerte.insert(i,value)
 			i=i+1
-			
+
 # Lesen Sie die deserialisierten Messwerte als Variable ein
-LFeuchte = Messwerte[13]
-Ta = Messwerte[15]
-Taupunkt = Messwerte[17]
-Windgeschw = Messwerte [19]
-Luftdruck = Messwerte[21]
-NDmenge = Messwerte[23]
-Regentropfen = Messwerte[27]
-Tamb = Messwerte[30]
-Tir = Messwerte[31]
-Helligkeit = Messwerte[33]
-VISHelligkeit = Messwerte[34]
-IRHelligkeit= Messwerte[35]
+# Zugriff auf die benötigten Felder
+sunrise = raw_data['data'][0]['sunrise']
+sunset = raw_data['data'][0]['sunset']
+temp = raw_data['data'][0]['temp']
+pressure = raw_data['data'][0]['pressure']
+dewpoint = raw_data['data'][0]['dew_point']
+clouds = raw_data['data'][0]['clouds']
+windspeed = raw_data['data'][0]['wind_speed']
+weather_desc = raw_data['data'][0]['weather'][0]['description']
+
+# Überprüfen, ob das 'rain'-Feld existiert
+if 'rain' in raw_data['data'][0]:
+    rain_1h = raw_data['data'][0]['rain']['1h']
+else:
+    rain_1h = None
+    
+# Umwandeln der Unix-Zeitstempel in Datetime-Objekte
+sunrise_utc = datetime.fromtimestamp(sunrise, timezone.utc)
+sunset_utc = datetime.fromtimestamp(sunset, timezone.utc)
+utc_offset = timedelta(hours=2)
+sunrise_utc2 = sunrise_utc.astimezone(timezone(utc_offset))
+sunset_utc2 = sunset_utc.astimezone(timezone(utc_offset))
+# Umwandeln in das gewünschte Zeitformat (z.B. 'YYYY-MM-DD HH:MM:SS')
+sunrise_formatted = sunrise_utc2.strftime('%H:%M:%S')
+sunset_formatted = sunset_utc2.strftime('%H:%M:%S')
+
+
+Ta = Messwerte[12]
+Taupunkt = dewpoint
+Windgeschw = windspeed
+Luftdruck = pressure
+Bewölkung = clouds
+Wetter = weather_desc
+NDmenge = rain_1h
+Regentropfen = Messwerte[2]
+Tamb = Messwerte[5]
+Tir = Messwerte[6]
+Helligkeit = Messwerte[8]
+VISHelligkeit = Messwerte[9]
+IRHelligkeit= Messwerte[10]
 SQM = calFac + (log((Helligkeit/108000.0),10)/-0.4)
+
 #Wolkenbedeckung
 if abs((K2/10-Tamb)) < 1:
     T67= sign(K6)*sign(Tamb-K2/10)*abs((K2/10-Tamb))
@@ -103,21 +127,23 @@ else:
 Td=(K1/100)*(Tamb-K2/10)+(K3/100)*pow(exp(K4/1000*Tamb),(K5/100))+T67
 Tsky=Tir-Td
 Cldcov=100+((Tsky/8)*100)
-print(format(Tsky,".2f"),format(Cldcov,".2f"),format(Ta,".2f"),format(Tamb,".2f"),format(Tir,".2f"))
+# print(format(Tsky,".2f"),format(Cldcov,".2f"),format(Ta,".2f"),format(Tamb,".2f"),format(Tir,".2f"))
 
 # CSV-Datei zum Schreiben öffnen
 with open(dateiname, mode='w', newline='') as datei:
             schreiber = csv.writer(datei)
-            schreiber.writerow(["Temp/C: ", Ta])
+            schreiber.writerow(["Sonnenaufgang: ", sunrise_formatted])
+            schreiber.writerow(["Wetter: ",Wetter])
+            schreiber.writerow(["Temp/C: ", temp])
             schreiber.writerow(["Druck/hPa: ", Luftdruck])
-            schreiber.writerow(["Feuchte/%: ", LFeuchte])
             schreiber.writerow(["TauPkt/C: ", Taupunkt])
+            schreiber.writerow(["Bewölkung/%: ", Bewölkung])
             schreiber.writerow(["WindV/m/s: ", Windgeschw])
             schreiber.writerow(["SQM/mag/bs² :",format (SQM,".1f")])
-            schreiber.writerow(["Wolken/ % :", format (Cldcov,".1f")])
+            schreiber.writerow(["Regen in 1h :", format (rain_1h)])
             schreiber.writerow(["NDmenge/mm/m²: ", NDmenge])
-            #schreiber.writerow(["Tropfen: ", Regentropfen])
+            schreiber.writerow(["Sonnenuntergang: ", sunset_formatted])
+            schreiber.writerow(["Tropfen: ", Regentropfen])
       
-  	
-#print('Alles fertig')
 
+#print('Alles fertig')
